760521770dd525ac9c79b434655a1a73a358a954
David Weiser <davidann@moz.com>
2016-05-04 08:10:00 -0700

    initial commit

---
+++ src/fwpd/core.clj
@@ -0,0 +1,15 @@
+(ns fwpd.core)
+
+(def filename "suspects.csv")
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity :glitter-index str->int})
+
+(defn convert
+  [vamp-key value[]
+   ((get conversions vamp-key) value))
